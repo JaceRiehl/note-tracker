35f831d56d371b49a3bc0dbd8882724e24dac468
JaceRiehl <jace.riehl@gmail.com>
2019-07-17 20:00:54 -0600

    Added firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB973zuixqbWm3Y3RCd6wZ6JN09Oyhcl8s",
+    authDomain: "notekeeper-82146.firebaseapp.com",
+    databaseURL: "https://notekeeper-82146.firebaseio.com",
+    projectId: "notekeeper-82146",
+    storageBucket: "",
+    messagingSenderId: "472750000898",
+    appId: "1:472750000898:web:35291c9af29e41d4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
